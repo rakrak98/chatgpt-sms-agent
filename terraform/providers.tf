9560e0c734cb6b44a22f581488db8279a627df5b
rakrak98 <rakib.kamal42@gmail.com>
2023-01-15 22:52:52 -0500

    Adding terraform init with s3 bucket state file management

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+    backend "s3" {
+        bucket = var.bucket_name
+        key = var.tf_state_key
+        region = "us-east-1"
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"

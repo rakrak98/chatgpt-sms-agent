fa4bb2da942a4457fe20415870b771474bb4cf97
rakrak98 <rakib.kamal42@gmail.com>
2023-01-16 17:35:49 -0500

    Adding more logs to determine data parsing mechanism

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 exports.handler = async function(context, event, callback) {
     const twiml = new MessagingResponse();
-    const inBoundMsg = event;
+    const inBoundMsg = Buffer.from(event["body"], "base64");
+    const msgBody = inBoundMsg["Body"];
+    const fromPhoneNumber = inBoundMsg["From"];
     // const configuration = new Configuration({
     //     apiKey: process.env.OPENAI_API_KEY
     // });
@@ -15,8 +17,9 @@ exports.handler = async function(context, event, callback) {
     //     frequency_penalty: 0.7
     // });
     // twiml.message(response.data.choices[0].text);
-    console.log("Message Received in the logs!");
-    console.log(context);
-    console.log(event);
+    console.log("Message Received in the logs!: ", msgBody);
+    console.log("From: ", fromPhoneNumber);
+    console.log("Context: ", context);
+    console.log("Event:", event);
     callback(null, "Message Received");
 }

6dcf82ce4a3bfc4c57648951eb20dc740055229c
rakrak98 <rakib.kamal42@gmail.com>
2023-01-15 23:41:21 -0500

    Adding terraform fmt and lowering size of zipped source code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "sms_processing_function" {
-  function_name = "SMSProcessingFunction"
-  role          = aws_iam_role.sms_processing_function_role.arn
-  handler       = "index.js"
-  runtime = "nodejs18.x"
-  filename = "function.zip"
+  function_name    = "SMSProcessingFunction"
+  role             = aws_iam_role.sms_processing_function_role.arn
+  handler          = "index.js"
+  runtime          = "nodejs18.x"
+  filename         = "function.zip"
   source_code_hash = filebase64sha256("function.zip")
 }
 resource "aws_iam_role" "sms_processing_function_role" {
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-        region = "us-east-1"
-    }
+  backend "s3" {
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
